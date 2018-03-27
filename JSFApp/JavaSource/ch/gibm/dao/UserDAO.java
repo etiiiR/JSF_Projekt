2f724336e74e78531f4e96abbb2b0ee49ca55b5b
Etiennes Macbook <supereti@gmx.ch>
2018-03-27 12:14:12 +0200

    Userfacade noch fehler
    Weiss nicht genau wie implementieren

--- JSFApp/JavaSource/ch/gibm/dao/UserDAO.java
+++ JSFApp/JavaSource/ch/gibm/dao/UserDAO.java
@@ -13,6 +13,10 @@ public class UserDAO extends GenericDAO<User> {
 	public UserDAO() {
 		super(User.class);
 	}
+
+	public void delete(User user) {
+        	super.delete(user.getId(), User.class);
+	}
 	
 
 	
--- JSFApp/JavaSource/ch/gibm/facade/UserFacade.java
+++ JSFApp/JavaSource/ch/gibm/facade/UserFacade.java
@@ -3,6 +3,7 @@ package ch.gibm.facade;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import ch.gibm.dao.EntityManagerHelper;
@@ -13,13 +14,45 @@ import ch.gibm.entity.Person;
 import ch.gibm.entity.User;
 
 public class UserFacade {
+	private UserDAO userDAO = new UserDAO();
+	
+	public void createUser(User user) {
+		EntityManagerHelper.beginTransaction();
+		userDAO.save(user);
+		EntityManagerHelper.commitAndCloseTransaction();
+	}
 
-	public User getUserByName(String name) {
-		// TODO Auto-generated method stub
-		
-		return user;
+	public void updateUser(User user) {
+		EntityManagerHelper.beginTransaction();
+		User persistedUser = userDAO.find(user.getId());
+		persistedUser.setUsername(user.getUsername());
+		EntityManagerHelper.commitAndCloseTransaction();
 	}
 	
+	public void deletePerson(User user){
+		EntityManagerHelper.beginTransaction();
+		User persistedUserWithIdOnly = userDAO.findReferenceOnly(user.getId());
+		userDAO.delete(persistedUserWithIdOnly);
+		EntityManagerHelper.commitAndCloseTransaction();
+		
+	}
+
+	public User findPerson(int id) {
+		EntityManagerHelper.beginTransaction();
+		User user = userDAO.find(id);
+		EntityManagerHelper.commitAndCloseTransaction();
+		return user;
+	}
+
+	public User getUserByName(String name) {
+		EntityManagerHelper.beginTransaction();
+		Query q1 = EntityManagerHelper.getEntityManager().createQuery("SELECT username FROM Users c");
+		List results = q1.getResultList();
+		EntityManagerHelper.commitAndCloseTransaction();
+		return (User) results;
+	}
+	
+
 
 }
 
