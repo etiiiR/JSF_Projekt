bbde1a885a8d5301d4b3e07a5e01caee72207d3f
thedi eti <etienne.roulet@gibmit.ch>
2018-04-09 22:18:26 +0200

    Testklasse final
    Testklasse final

--- JSFApp/JavaSource/ch/gibm/test/TestUser.java
+++ JSFApp/JavaSource/ch/gibm/test/TestUser.java
@@ -21,8 +21,8 @@ public class TestUser {
 		EntityManager entity = EntityManagerHelper.getEntityManager();
 		entity.getTransaction().begin();
 		User user = new User();
-		user.setName("Etienne");
-		user.setPass("Roulet");
+		user.setName("User");
+		user.setPass("User");
 		entity.persist(user);
 		entity.getTransaction().commit();
 		
@@ -30,10 +30,44 @@ public class TestUser {
 		EntityManager entity2 = EntityManagerHelper.getEntityManager();
 		entity2.getTransaction().begin();
 		Role role = new Role();
-		role.setRole_name("ADMIN");
+		role.setRole_name("USERS");
 		role.setUser(user);
 		entity2.persist(role);
 		entity2.getTransaction().commit();
+		
+		EntityManager entity3 = EntityManagerHelper.getEntityManager();
+		entity2.getTransaction().begin();
+		User user2 = new User();
+		user2.setName("Admin");
+		user2.setPass("Admin");
+		entity3.persist(user2);
+		entity3.getTransaction().commit();
+		
+		
+		EntityManager entity4 = EntityManagerHelper.getEntityManager();
+		entity4.getTransaction().begin();
+		Role role2 = new Role();
+		role2.setRole_name("ADMIN");
+		role2.setUser(user2);
+		entity4.persist(role2);
+		entity4.getTransaction().commit();
+		
+		EntityManager entity5 = EntityManagerHelper.getEntityManager();
+		entity5.getTransaction().begin();
+		User user3 = new User();
+		user3.setName("Owner");
+		user3.setPass("Onwer");
+		entity5.persist(user3);
+		entity5.getTransaction().commit();
+		
+		
+		EntityManager entity6 = EntityManagerHelper.getEntityManager();
+		entity6.getTransaction().begin();
+		Role role3 = new Role();
+		role3.setRole_name("OWNER");
+		role3.setUser(user3);
+		entity6.persist(role3);
+		entity6.getTransaction().commit();
 		
 		
 		
